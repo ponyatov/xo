60d8b98af153cb1da1e2aada8f89cde5c3ce0ef4
Dmitry Ponyatov <dponyatov@gmail.com>
2017-02-01 13:07:30 +0400

    + Sym tree
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- py.py
+++ py.py
@@ -1,3 +1,13 @@
+################ Sym tree ################
+
+class Sym:
+    tag = 'sym'
+    def __init__(self, V): self.val = V
+    def __repr__(self): return self.head()
+    def head(self): return '<%s:%s>' % (self.tag, self.val)
+class Op(Sym):
+    tag = 'op'
+
 ################ parser ################
 
 import sys
@@ -7,20 +17,20 @@ import ply.yacc as yacc
 tokens = [ 'SYM' , 'OP' ]
 
 t_ignore_comment = r'\#.*'
-t_ignore  = ' \t\r'
+t_ignore = ' \t\r'
 def t_newline(tok):
     r'\n'
     tok.lexer.lineno += 1
     
 def t_SYM(tok):
     r'[a-zA-Z0-9_.]+|@gvim|-p'
-    return tok
+    tok.value = Sym(tok.value) ; return tok
 def t_OP(tok):
     r'[\=\@\+\-\*\/\^\[\]]'
-    return tok    
+    tok.value = Op(tok.value) ; return tok
 
-def t_error(tok): print tok.lexer,'error',tok
+def t_error(tok): print tok.lexer, 'error', tok
 
 lexer = lex.lex()
 lexer.input(sys.stdin.read())
-for i in iter(lexer.token,None): print i
+for i in iter(lexer.token, None): print i
